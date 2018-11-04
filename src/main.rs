bb162b585cdd9bfc6d16f6264eb05b4e2bb6de86
Chris Butler <chrisbutler296@gmail.com>
2018-11-04 17:58:51 -0500

    Updated main to use Float3

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ use std::{
 use image;
 
 mod geometry;
+use self::geometry::{
+    Float,
+    Float3
+};
 
 fn write_image(filename: &str) -> io::Result<()> {
     let nx = 200;
@@ -15,18 +19,21 @@ fn write_image(filename: &str) -> io::Result<()> {
 
     let mut imgbuf = image::RgbImage::new(nx, ny);
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let r = x as f32 / nx as f32;
-        let g = y as f32 / ny as f32;
-        let b = 0.2_f32;
+        let mut rgb = Float3 {
+            x: x as Float / nx as Float,
+            y: y as Float / ny as Float,
+            z: 0.2,
+        };
+        assert!(rgb.x.abs() <= 1.0);
+        assert!(rgb.y.abs() <= 1.0);
+        assert!(rgb.z.abs() <= 1.0);
+        rgb *= 255.99;
 
-        assert!(r.abs() <= 1.0);
-        assert!(g.abs() <= 1.0);
-        assert!(b.abs() <= 1.0);
-
-        let ir = (255.99 * r) as u8;
-        let ig = (255.99 * g) as u8;
-        let ib = (255.99 * b) as u8;
-        *pixel = image::Rgb([ir, ig, ib]);
+        *pixel = image::Rgb([
+            rgb.x as u8,
+            rgb.y as u8,
+            rgb.z as u8,
+        ]);
     }
 
     imgbuf.save(filename)?;
