e2250117aa9f7d4117800c222517e1ffc555f091
Chris Butler <chrisbutler296@gmail.com>
2018-11-04 10:46:07 -0500

    ppm -> png

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,32 @@ use std::{
     io,
 };
 
-fn write_p3(writer: &mut impl io::Write) -> io::Result<()> {
+use image;
+
+fn write_image(filename: &str) -> io::Result<()> {
     let nx = 200;
     let ny = 100;
-    writeln!(writer, "P3")?;
-    writeln!(writer, "{} {}", nx, ny)?;
-    writeln!(writer, "255")?;
 
-    for j in (0..ny).rev() {
-        let j = j as f32;
-        for i in 0..nx {
-            let i = i as f32;
-            let r = i / nx as f32;
-            let g = j / ny as f32;
-            let b = 0.2;
+    let mut imgbuf = image::RgbImage::new(nx, ny);
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let r = x as f32 / nx as f32;
+        let g = y as f32 / ny as f32;
+        let b = 0.2_f32;
 
-            let ir = (255.99 * r) as u32;
-            let ig = (255.99 * g) as u32;
-            let ib = (255.99 * b) as u32;
+        assert!(r.abs() <= 1.0);
+        assert!(g.abs() <= 1.0);
+        assert!(b.abs() <= 1.0);
 
-            writeln!(writer, "{} {} {}", ir, ig, ib)?;
-        }
+        let ir = (255.99 * r) as u8;
+        let ig = (255.99 * g) as u8;
+        let ib = (255.99 * b) as u8;
+        *pixel = image::Rgb([ir, ig, ib]);
     }
 
+    imgbuf.save(filename)?;
     Ok(())
 }
 
 fn main() {
-    let mut stdout = std::io::stdout();
-    write_p3(&mut stdout).unwrap();
+    write_image("output.png").unwrap();
 }
