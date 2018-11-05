44dead2e36e4f53e801b9a082e5459be30b8e1f2
Chris Butler <chrisbutler296@gmail.com>
2018-11-04 22:48:51 -0500

    Prepare Lambertian to probabilistically scatter

--- src/material.rs
+++ src/material.rs
@@ -4,8 +4,8 @@ use crate::float3::*;
 use crate::hitable::*;
 
 pub struct ScatterResult {
-    attenuation: Float3,
-    scattered:   Ray,
+    pub attenuation: Float3,
+    pub scattered:   Option<Ray>,
 }
 
 pub trait Material: std::fmt::Debug {
@@ -14,14 +14,15 @@ pub trait Material: std::fmt::Debug {
 
 #[derive(Copy, Clone, Debug, Default)]
 struct Lambertian {
-    albedo: Float3,
+    pub albedo: Float3,
 }
 
 impl Material for Lambertian {
-    fn scatter(&self, ray_in: &Ray, record: &HitRecord) -> ScatterResult {
+    fn scatter(&self, _ray_in: &Ray, record: &HitRecord) -> ScatterResult {
         let target = record.p + record.normal + Float3::random_in_sphere();
+        let scattered = Some(Ray { origin: record.p, dir: target - record.p });
         ScatterResult {
-            scattered:   Ray { origin: record.p, dir: target - record.p },
+            scattered,
             attenuation: self.albedo,
         }
     }
