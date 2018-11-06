32d19e12f52518ed194f72e5308c0f9cdbd880fe
Chris Butler <chrisbutler296@gmail.com>
2018-11-06 00:25:17 -0500

    Added Camera::new()

--- src/camera.rs
+++ src/camera.rs
@@ -13,6 +13,18 @@ pub struct Camera {
 }
 
 impl Camera {
+    pub fn new(vfov: Float, aspect: Float) -> Camera {
+        let theta = vfov * std::f64::consts::PI / 180.0;
+        let half_height = (theta / 2.0).tan();
+        let half_width = aspect * half_height;
+        Camera {
+            lower_left: Float3::xyz(-half_width, -half_height, -1.0),
+            horizontal: Float3::xyz(2.0*half_width, 0.0, 0.0),
+            vertical:   Float3::xyz(0.0, 2.0*half_height, 0.0),
+            origin:     Float3::xyz(0, 0, 0),
+        }
+    }
+
     pub fn get_ray(&self, u: Float, v: Float) -> Ray {
         Ray {
             origin: self.origin,
