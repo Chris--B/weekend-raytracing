a28fbc6265794608c0461633026cbb975e5e2206
Chris Butler <chrisbutler296@gmail.com>
2018-11-05 22:43:29 -0500

    Added schlick()

--- src/ray.rs
+++ src/ray.rs
@@ -15,3 +15,11 @@ impl Ray {
         self.origin + t * self.dir
     }
 }
+
+/// When you look at a window at a steep angle, it becomes a mirror.
+/// This is a simple approximation to that by Christophe Schlick.
+pub fn schlick(cosine: Float, refraction_index: Float) -> Float {
+    let mut r0 = (1.0 - refraction_index) / (1.0 + refraction_index);
+    r0 *= r0;
+    r0 + (1.0 - r0)*(1.0 - cosine).powi(5)
+}
